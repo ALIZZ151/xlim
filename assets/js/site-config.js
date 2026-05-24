85f46a17dda30af03f0b19639ee4c03a7c300b44
ALIZZ151 <radenrustam08@gmail.com>
2026-05-24 09:23:24 +0700

    Add files via upload

--- assets/js/site-config.js
+++ assets/js/site-config.js
@@ -1,11 +1,12 @@
 window.XLIM_CONFIG = {
   siteName: 'XLIMSTORE',
-  siteUrl: 'https://admin-xlim.alizz.my.id',
-  apiBaseUrl: 'https://xlim.alizz.my.id',
+  siteUrl: 'https://xlim.alizz.my.id',
+  // Domain ini adalah backend/API Pterodactyl. Dari screenshot, admin-xlim mengarah ke Express backend.
+  apiBaseUrl: 'https://admin-xlim.alizz.my.id',
   whatsappNumber: '6283193075449',
   social: {
     telegram: 'https://t.me/xlimstor',
     instagram: 'https://www.instagram.com/xlim.str.id?igsh=dmJ5dng4OGFxdGR6'
   },
-  fallbackImage: '/assets/img/product-fallback.svg'
+  fallbackImage: 'assets/img/product-fallback.svg'
 };
