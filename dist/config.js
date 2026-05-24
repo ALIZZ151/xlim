525d916534ce9ccddbc20d9483af6b19fab149c2
ALIZZ151 <radenrustam08@gmail.com>
2026-05-24 14:12:20 +0700

    Update config.js

--- dist/config.js
+++ dist/config.js
@@ -1,5 +1,5 @@
 window.XLIMSTORE_CONFIG={
   "API_BASE_URL": "https://admin-xlim.alizz.my.id",
-  "WHATSAPP_NUMBER": "628xxxxxxxxxx",
+  "WHATSAPP_NUMBER": "6283193075449",
   "SITE_URL": "https://xlim.alizz.my.id"
 };
